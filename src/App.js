93db66ad34c3aab5ab3f7bff3fb53f83375b8e9c
Andrew Rozs <rozs0001@hz.nl>
2024-01-21 18:21:35 +0100

    changes in develop

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1>React To-Do List!</h1>
+      <h1>My Awesome To-Do List!</h1>
       <input
         type="text"
         value={newTask}
